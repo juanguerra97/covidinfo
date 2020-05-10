1d3991260f8210548cc00392b70ebb7fd619d222
Juan Guerra <juanguerra97@protonmail.com>
2020-05-10 14:09:21 -0600

    Context de la base de datos

---
+++ CovidInfoWebService/DataAccess/InfoCovidDbContext.cs
@@ -0,0 +1,44 @@
+﻿using CovidInfoWebService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CovidInfoWebService.DataAccess
+{
+    public class InfoCovidDbContext : DbContext
+    {
+        
+        public DbSet<CasoCovid> CasosCovid { get; set; }
+
+        public InfoCovidDbContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Localizacion>(loc =>
+            {
+                loc.Property(l => l.LocalizacionId).ValueGeneratedOnAdd();
+                loc.Property(l => l.Pais).IsRequired().HasMaxLength(128);
+                loc.Property(l => l.Departamento).IsRequired().HasMaxLength(128);
+                loc.Property(l => l.Municipio).IsRequired().HasMaxLength(128);
+            });
+
+            modelBuilder.Entity<CasoCovid>(caso =>
+            {
+                caso.Property(c => c.CasoCovidId).ValueGeneratedOnAdd();
+                caso.HasOne(c => c.Localizacion);
+                caso.Property(c => c.PrimerNombre).IsRequired().HasMaxLength(64);
+                caso.Property(c => c.SegundoNombre).IsRequired().HasMaxLength(64);
+                caso.Property(c => c.PrimerApellido).IsRequired().HasMaxLength(64);
+                caso.Property(c => c.SegundoApellido).IsRequired().HasMaxLength(64);
+                caso.Property(c => c.Edad).IsRequired();
+                caso.Property(c => c.Sexo).IsRequired();
+                caso.Property(c => c.Fecha).IsRequired();
+            });
+
+        }
+
+    }
+}
