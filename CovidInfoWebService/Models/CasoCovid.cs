02f4ae3e6955468741c70707fcbdaf5e8974f148
Juan Guerra <juanguerra97@protonmail.com>
2020-05-10 14:07:50 -0600

    Modelos para representar los datos

---
+++ CovidInfoWebService/Models/CasoCovid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidInfoWebService.Models
+{
+
+    /// <summary>
+    /// Los objetos de esta clase almacenan los datos de los casos reportados de Covid 19.
+    /// </summary>
+    public class CasoCovid
+    {
+
+        public int CasoCovidId { get; set; }
+
+        public Localizacion Localizacion { get; set; }
+
+        public string PrimerNombre { get; set; }
+
+        public string SegundoNombre { get; set; }
+
+        public string PrimerApellido { get; set; }
+
+        public string SegundoApellido { get; set; }
+
+        public byte Edad { get; set; }
+
+        public char Sexo { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+
+    }
+}
---
+++ CovidInfoWebService/Models/Localizacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidInfoWebService.Models
+{
+
+    /// <summary>
+    /// Los objetos de esta clase almacenan los datos de Pais, Departamento y Municipio de cada caso de Covid 19 reportado
+    /// </summary>
+    public class Localizacion
+    {
+
+        public int LocalizacionId { get; set; }
+
+        public string Pais { get; set; }
+
+        public string Departamento { get; set; }
+
+        public string Municipio { get; set; }
+
+    }
+}
