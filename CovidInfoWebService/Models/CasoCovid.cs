536733e3d3edb507103f23fb9821442a37956fcf
Juan Guerra <juanguerra97@protonmail.com>
2020-05-12 12:57:31 -0600

    decorators para validar el modelo

--- CovidInfoWebService/Models/CasoCovid.cs
+++ CovidInfoWebService/Models/CasoCovid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,26 +15,45 @@ namespace CovidInfoWebService.Models
 
         public int CasoCovidId { get; set; }
 
+        [Required(ErrorMessage = "Falta el pais")]
+        [MaxLength(128, ErrorMessage = "La longitud maxima de {0} es {1}")]
         public string Pais { get; set; }
 
+        [Required(ErrorMessage = "Falta el departamento")]
+        [MaxLength(128, ErrorMessage = "La longitud maxima de {0} es {1}")]
         public string Departamento { get; set; }
 
+        [Required(ErrorMessage = "Falta el municipio")]
+        [MaxLength(128, ErrorMessage = "La longitud maxima de {0} es {1}")]
         public string Municipio { get; set; }
 
+        [Required(ErrorMessage = "Falta el primer nombre")]
+        [MaxLength(64, ErrorMessage = "La longitud maxima de {0} es {1}")]
         public string PrimerNombre { get; set; }
 
+        [Required(ErrorMessage = "Falta el segundo nombre")]
+        [MaxLength(64, ErrorMessage = "La longitud maxima de {0} es {1}")]
         public string SegundoNombre { get; set; }
 
+        [Required(ErrorMessage = "Falta el primer apellido")]
+        [MaxLength(64, ErrorMessage = "La longitud maxima de {0} es {1}")]
         public string PrimerApellido { get; set; }
 
+        [Required(ErrorMessage = "Falta el segundo apellido")]
+        [MaxLength(64, ErrorMessage = "La longitud maxima de {0} es {1}")]
         public string SegundoApellido { get; set; }
 
+        [Required(ErrorMessage = "Falta la edad")]
+        [Range(1, 120, ErrorMessage = "La edad debe estar entre {1} y {2}")]
         public byte Edad { get; set; }
 
+        [Required(ErrorMessage = "Falta el sexo")]
         public char Sexo { get; set; }
 
+        [Required(ErrorMessage = "Falta la fecha")]
         public DateTime Fecha { get; set; }
 
 
     }
+
 }
