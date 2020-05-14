b2f5674a476091115fbf1f02da5ea4ca38ef7d5a
Juan Guerra <juanguerra97@protonmail.com>
2020-05-14 11:57:41 -0600

    creacion y prueba de metodo para extraer url base de una url

---
+++ CovidInfoUnitTests/UrlExtractorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using CovidInfoWebService.Utils;
+
+namespace CovidInfoUnitTests
+{
+    public class UrlExtractorTest
+    {
+
+        [Theory]
+        [InlineData("https://localhost:44327", "https://localhost:44327/api/activo")]
+        [InlineData("https://127.0.0.1:80", "https://127.0.0.1:80/index.html")]
+        [InlineData("http://localhost", "http://localhost/api/casos/1")]
+        public void ExtractBaseUrlOk(string baseUrlExpected, string url)
+        {
+
+            Assert.Equal(baseUrlExpected, UrlExtractor.ExtractBaseUrl(url));
+
+        }
+
+    }
+}
---
+++ CovidInfoWebService/UrlExtractor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidInfoWebService.Utils
+{
+    public static class UrlExtractor
+    {
+
+        /// <summary>
+        /// Metodo para extraer el dominio del servidor de una url
+        /// </summary>
+        /// <param name="url">url de la que se extraera el dominio</param>
+        /// <returns></returns>
+        public static string ExtractBaseUrl(string url)
+        {
+
+            var regex = new Regex(@"^(https?://[^/\:\\]+(\:\d+)?).*$", RegexOptions.IgnoreCase);
+            var match = regex.Match(url);
+
+            if (!match.Success)
+            {
+                return null;
+            }
+
+            return match.Groups[1].Value;
+
+        }
+
+    }
+}
